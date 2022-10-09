4188eab36355c9ed7df08ea5d5aef4da155c1e48
Isaac Arnold <isaacarnold@icloud.com>
2022-10-09 19:59:31 +1000

    Adding navbar file

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Layout from "../components/Layout";
+import Navbar from "../components/Navbar";
 
 const HomePage = () => {
   return (
     <Layout>
-      {/* Navbar */}
+      <Navbar />
       {/* Title */}
       {/* Content */}
       {/* Footer */}
